ba70c2f9caef6ce70ab002c35baeddd8d7ea9697
Lucas Santos <lucas201273@gmail.com>
2024-07-21 09:11:34 -0300

    feat: add lib common project

---
+++ hd-commons-lib/src/main/java/models/enums/ProfileEnum.java
@@ -0,0 +1,29 @@
+package models.enums;
+
+
+import java.util.Arrays;
+
+public enum ProfileEnum {
+    ROLE_ADMIN("ROLE_ADMIN"),
+    ROLE_CUSTOMER("ROLE_CUSTOMER"),
+    ROLE_TECHNICIAN("ROLE_TECHNICIAN");
+
+    private final String description;
+
+    ProfileEnum(String description){
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static ProfileEnum toEnum(String description) throws IllegalAccessException {
+      return Arrays.stream(ProfileEnum.values())
+              .filter(profileEnum -> profileEnum.getDescription()
+              .equals(description)).findFirst()
+              .orElseThrow(()-> new IllegalAccessException("Invalid description: " + description));
+    }
+}
+
+
